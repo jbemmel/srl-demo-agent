5fc2bab877fe83fc52e42b13c8bce04eee9b3ee1
J vanBemmel <jvb127@gmail.com>
2021-04-08 17:20:31 -0500

    Update demo_fib_agent.sh

--- demo_fib_agent.sh
+++ demo_fib_agent.sh
@@ -17,12 +17,12 @@ function main()
 {
     trap _term SIGTERM
     local virtual_env="/opt/srlinux/python/virtual-env/bin/activate"
-    local main_module="/etc/opt/srlinux/appmgr/user_agents/demo_fib_agent.py"
+    local main_module="/etc/opt/srlinux/appmgr/demo_fib_agent.py"
 
     # source the virtual-environment, which is used to ensure the correct python packages are installed,
     # and the correct python version is used
     source "${virtual_env}"
-    export  PYTHONPATH="$PYTHONPATH:/etc/opt/srlinux/appmgr/user_agents:/opt/srlinux/bin:/opt/rh/rh-python36/root/usr/lib/python3.6/site-packages/sdk_protos"
+    export  PYTHONPATH="$PYTHONPATH:/etc/opt/srlinux/appmgr/:/opt/srlinux/bin:/opt/rh/rh-python36/root/usr/lib/python3.6/site-packages/sdk_protos"
 
     export http_proxy=""
     export https_proxy=""
